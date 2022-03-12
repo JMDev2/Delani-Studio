5e821dd97c4ee54f89b567fcb3b1882e5948052c
JOSEPHMAINA1995 <mainajoseph964@gmail.com>
2022-03-12 15:20:05 +0300

    ft:This will edit the about use section text

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 $(document).ready(()=>{
-    $("h1").click(()=>{
-        alert("You are set")
+    $(".icon1").click(()=>{
+        $(".text1").toggle();
+        
     })
+    $(".text1").click(()=>{
+        $(".icon1").toggle();
+    })
+    $(".icon3").click(()=>{
+        $(".text3").toggle();
+    })
+
+
+
+
+
+
+})
+$("form#form").submit((e)=>{
+    e.preventDefault();
+    let userName = $("input#fname").val();
+    let userEmail = $("input#email").val();
+    let userText = $("input#textarea").val();
+
+   alert(`${userName}, we have received your message. Thank you fro reaching out to us.`)
 })
